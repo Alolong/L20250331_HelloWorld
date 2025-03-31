f1307608018e8c1280a614f4353fa4f6dde1afff
Alolong <ddten123@gmail.com>
2025-03-31 17:51:14 +0900

    코드 연습
    for 문 추가

--- FileName.cpp
+++ FileName.cpp
@@ -28,7 +28,17 @@ int main()
 	float Number3 = (float)Number / (float)Number2;
 	cout << Number3 << endl;
 
-	cout << C << endl; // 1이다 
+
+
+	for (int i = 0; i < 10; i++) {
+		if (i == 5) break;
+		cout << i << endl;
+	}
+	cout << C << endl; // 1이다
+
+	
+
+
 	return 0;
 
 
